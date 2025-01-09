e3af99c0c7f41c0327bf09889813342b7db2e962
Josef Lange <josef.d.lange@gmail.com>
2025-01-08 21:31:43 -0600

    more logging

--- src/kiaconnect/client.ts
+++ src/kiaconnect/client.ts
@@ -50,7 +50,7 @@ export class KiaConnect {
     );
 
     this.log.debug('getTransactionStatus', res.data);
-    this.log.debug('getTransactionStatus', JSON.stringify(res.data));
+    this.log.debug('getTransactionStatus2', JSON.stringify(res.data));
 
     // If the transaction is no longer being remotely executed...
     return res.data.payload.remoteStatus === 0;
@@ -203,6 +203,7 @@ export class KiaConnect {
     );
 
     this.log.debug('vehicleInfo', res.data);
+    this.log.debug('vehicleInfo', JSON.stringify(res.data));
 
     const data = res.data as VehicleInfoResponse;
     const info = data.payload.vehicleInfoList.find((info) => info.vehicleConfig.vehicleDetail.vehicle.vin === vin);
